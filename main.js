ecbc4d68fcb85e13939dc4f12b4a1cac65313c7e
Dawid <dawidwas155@gmail.com>
2024-04-15 19:30:09 -0500

    Explanations

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+//------------DATABASE SECTION: Grabs all the functions from the database api that we need and gets the database------------\\
+// Note: DO NOT change any of the code in the database section as doing so will mess up accessing your database
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { get, set, getDatabase, ref} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCcIOL_iI101QnQkR_fa2OMHXKaxWw-RS0",
   authDomain: "readwrite-451d0.firebaseapp.com",
@@ -14,50 +12,66 @@ const firebaseConfig = {
   appId: "1:24459142527:web:42c121dc8612525f35664a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
-// Elements
-const connectionLabel = document.getElementById("connectionLabel")
-const keyInput = document.getElementById("keyInput")
-const valueInput = document.getElementById("valueInput")
-const setButton = document.getElementById("setButton")
-const getButton = document.getElementById("getButton")
+//------------ELEMENTS SECTION: These are all the html elements on our project that we'll need------------\\
+const connectionLabel = document.getElementById("connectionLabel") // Displays what database we're connected to
+const keyInput = document.getElementById("keyInput") // Enter in the key here
+const valueInput = document.getElementById("valueInput") // Enter in the key value here
+const setButton = document.getElementById("setButton") // Click on this button to set the value of a key
+const getButton = document.getElementById("getButton") // Click on this button to get the value of a key
 
-// Database Connection
-if(db){
-    connectionLabel.innerText = "Database Connection: " + firebaseConfig.projectId
+//------------CONNECTIVITY SECTION: Code that changes the "connectionLabel" text if we're connected to our database------------\\
+if(db){ // Checks if the database, db, exists
+    connectionLabel.innerText = "Database Connection: " + firebaseConfig.projectId // The project id contains the name of our database
 } else {
     connectionLabel.innerText = " Database Connection: Not Connected"
 }
 
-// Writing Data
-setButton.addEventListener("click", function(){
-  if(keyInput.value != "" && valueInput.value != ""){
-      set(ref(db, "data/" + keyInput.value), {
-          value: valueInput.value
-      }).then(function(){
-          alert("Successfully set the value of key '" + keyInput.value + "' to '" + valueInput.value + "'")
-      }).catch(function(err){
-          alert("Failed to update. Error: " + err)
-      })
-  } else {
-      alert("You must enter a string into both the key and value boxes.")
-  }
-})
+//------------WRITING SECTION: Allows us to CHANGE values in our database------------\\
+setButton.addEventListener("click", function(){ // Detect clicks of the set button
+    if(keyInput.value != "" && valueInput.value != ""){ // Checks if both input boxes have something in them
+        /*
+            ref() is a function that gives us a reference to a specific part of our database. This allows the set method to change 
+            our desired value. 
+        
+            In this case, we want the set function to look into our database and change the value with the key of "keyInput.value" 
+            to "valueInput.value". We also want to do this in a sub section called data, which is what the "data/ +" is for.
 
-// Setting Data
-getButton.addEventListener("click", function(){
-    if(keyInput.value != ""){
-        get(ref(db, "data/" + keyInput.value)).then(function(snapshot){
-            const data = snapshot.val()
-            valueInput.value = data.value  
-        }).catch(function(err){
-            valueInput.value = ""
-            alert("Failed to get data. Error: " + err)
+            ALSO if you put in a key that doesn't exist in the database already, Firebase will automatically create a new key with
+            the value of "valueInput.value." Pretty nice!
+        */
+        set(ref(db, "data/" + keyInput.value), { 
+            value: valueInput.value
+        }).then(function(){ // .then() calls a function only IF the set() method succeeds
+            alert("Successfully set the value of key '" + keyInput.value + "' to '" + valueInput.value + "'") // Success message
+        }).catch(function(err){ // .catch calls a function IF set() fails. It also gives us the error
+            alert("Failed to update. Error: " + err) // Error message
         })
     } else {
-        alert("You must enter a string into the key box.")
+        alert("You must enter a string into both the key and value boxes.") // This alerts us if we have nothing written in the boxes.
     }
 })
+
+//------------READING SECTION: Allows us to SEE values in our database------------\\
+getButton.addEventListener("click", function(){ // Detect clicks of the get button
+    if(keyInput.value != ""){ // Checks if the key input box have something in it
+        /*
+            Again, we use the ref() method mentioned earlier in union with the .then() method. When using the get() 
+            method, we recieve a "snapshot" of the data provided. By calling snapshop.val(), we get a data object.
+            This data object holds a value property which tells us the value stored in the key. 
+            
+            Calling snapshot.val().value gets us our value stored in the key
+        */
+        get(ref(db, "data/" + keyInput.value)).then(function(snapshot){
+            valueInput.value = snapshot.val().value // Inserts the value into the value input box
+        }).catch(function(err){ // .catch calls a function IF set() fails. It also gives us the error
+            valueInput.value = ""
+            alert("Failed to get data. Error: " + err) // Error message
+        })
+    } else {
+        alert("You must enter a string into the key box.") // This alerts us if nothing is written in the key box
+    }
+})
+
